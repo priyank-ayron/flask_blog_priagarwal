af93322870ebc43434b6ec308c9bbfb50e05b9df
Priyank Agarwal <priyank.ayron@gmail.com>
2020-11-20 21:23:18 +0530

    Update __init__.py

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -5,6 +5,9 @@ from datetime import datetime
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'thisisasecret'
+
 db = SQLAlchemy(app)
 
 
